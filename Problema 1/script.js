d0ecfbfab2ab128fd9c02efa154c4c3a7c396c72
AndreeaSamoila <a.samoilaa@yahoo.com>
2023-01-11 14:31:55 +0200

    adding the exercises

---
+++ Problema 1/script.js
@@ -0,0 +1,20 @@
+//Problema 1 
+const a = 2 + 3 * 4 - 5;
+console.log("Rezultatul calculului 1 este: ", a);
+
+const b = 4 * (2 + 6 / 3);
+console.log("Rezultatul calculului 2 este: ", b);
+
+const c = 2 * 3 ** 2 + 2 ** 5;
+console.log("Rezultatul calculului 3 este: ", c);
+// Another solution:  const c = 2 * Math.pow(3, 2) + Math.pow(2, 5);   
+
+const d = (5 * (3 + 2/2)) + 3;
+console.log("Rezultatul calculului 4 este: ", d);
+
+
+const e = (2 ** 5 - 5 ** 2 + 3 * (5 - 4/2)) / ( 2 ** 5 + 5 ** 2);
+console.log("Rezultatul calculului 5 este: ", e);
+
+//Another solution: 
+// const e = (Math.pow(2,5) - Math.pow(5,2) + 3 * (5 - 4/2)) / ( Math.pow(2,5) + Math.pow(5,2));
---
+++ Problema 2/script.js
@@ -0,0 +1,10 @@
+const x = 5;
+const y = 8;
+
+//Ecuatia 1:
+const rezultat1 = 2 * x - 5 * y;
+console.log("Rezultatul ecuatiei este: ", rezultat1);
+
+//Ecuatia 2:
+const rezultat2 = 3 * x + 9 * y;
+console.log("Rezultatul ecuatiei este: ", rezultat2);
